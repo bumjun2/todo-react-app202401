947012b54e819ceef457e538b7f79fcef164d9fa
bumjun2 <jbj3713@naver.com>
2024-01-19 11:28:30 +0900

    리액트 라우터 돔으로 회원가입 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoTemplate from "./component/TodoTemplate";
-import ImageSetting from "./component/ImageSetting";
+import TodoTemplate from "./component/todo/TodoTemplate";
+import Header from "./component/layout/Header";
+import Footer from "./component/layout/Footer";
+import Join from "./component/user/Join";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <>
-        <TodoTemplate />
-        <ImageSetting />
+        <Header />
+        <Routes>
+            <Route path='/' element={ <TodoTemplate /> }/>
+            <Route path='/join' element={ <Join /> }/>
+        </Routes>
+        <Footer />
     </>
   );
 }
